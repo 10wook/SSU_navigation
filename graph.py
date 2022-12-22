c1b14afd82c996e9a763d6e7973d818242c66dca
김경훈 <54941130+ouvir@users.noreply.github.com>
2022-12-22 17:14:08 +0900

    Create graph.py
    feat graph.py
    
    networkx 라이브러리 이용
    그래프 위치 시각적 표현

---
+++ graph.py
@@ -0,0 +1,29 @@
+import networkx as nx
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.image as img
+
+# 이미지 불러오기
+image = img.imread('./image.jpg')
+
+# csv 데이터 불러오기
+df_p = pd.read_csv('./position_data.csv')
+
+# 그래프 객체 설정
+G = nx.Graph()
+
+# 노드 설정
+for i in range(len(df_p['point_name'])):
+    G.add_node(str(df_p['point_name'][i]))
+
+# 좌표 설정
+pos = {}
+for i in range(len(df_p['point_name'])):
+    node = df_p['point_name'][i]
+    pos[str(node)] = (df_p['pos_x'][i], df_p['pos_y'][i])
+
+# 그리기
+nx.draw(G, pos, node_size=100, font_size=5,
+        font_color="white", with_labels=True)
+plt.imshow(image)
+plt.show()
