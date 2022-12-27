b7e747234f7d25a09dc65c12faa609033b4e5b60
김경훈 <54941130+ouvir@users.noreply.github.com>
2022-12-28 03:17:48 +0900

    change path
    이미지 경로 변경에 따른 path 변경

--- dijkstra.py
+++ dijkstra.py
@@ -2,28 +2,26 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import matplotlib.image as img
 
-image = img.imread('./image.jpg')
+image = img.imread('./img/image.jpg')
 
-def shortpath_print_dijkstra(G,start,end):
-    tg = nx.dijkstra_path(G,start,end, weight='weight')
-    min = nx.dijkstra_path_length(G,end,start)
+
+def shortpath_print_dijkstra(G, start, end):
+    tg = nx.dijkstra_path(G, start, end, weight='weight')
+    min = nx.dijkstra_path_length(G, end, start)
     NG = nx.MultiDiGraph.copy(G)
-    nx.MultiDiGraph.clear_edges(NG)   
+    nx.MultiDiGraph.clear_edges(NG)
     for i in range(len(tg)-1):
-        NG.add_edge(tg[i],tg[i+1])
+        NG.add_edge(tg[i], tg[i+1])
     return NG, min
 
 
-def nxgraph_draw(Graph,pos):
-    
-    nx.draw_networkx_nodes(Graph, pos=pos,node_size=300,node_color='yellow')
+def nxgraph_draw(Graph, pos):
+
+    nx.draw_networkx_nodes(Graph, pos=pos, node_size=300, node_color='yellow')
     #nx.draw_networkx_edge_labels(Graph, pos=pos)
     nx.draw_networkx_edges(Graph, pos=pos)
     nx.draw_networkx_labels(Graph, pos=pos, font_size=10)
     plt.imshow(image)
     plt.show()
-    
-    
-    
 
-    nx.MultiDiGraph.clear_edges()
+    # nx.MultiDiGraph.clear_edges()
